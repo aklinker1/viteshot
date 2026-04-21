ddca3be0ef770e2e289a3657d80c1f2446f85e82
Aaron <aaronklinker1@gmail.com>
2026-04-21 13:25:28 -0500

    fix(dev): Use real package name when resolving assets dir

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,6 @@ import { resolve, dirname, join } from "node:path";
 export async function getViteshotAssetsDir(): Promise<string> {
   if (process.env.DEV) return resolve("assets");
 
-  const pkgJsonPath = await import.meta.resolve("viteshot/package.json");
+  const pkgJsonPath = await import.meta.resolve("@aklinker1/viteshot/package.json");
   return join(dirname(pkgJsonPath), "assets");
 }
