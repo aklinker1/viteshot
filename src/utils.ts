d29ddf3f07442f112c708194e17b3864e5143ff8
Aaron <aaronklinker1@gmail.com>
2026-04-21 13:27:20 -0500

    chore: Fix formatting

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,7 @@ import { resolve, dirname, join } from "node:path";
 export async function getViteshotAssetsDir(): Promise<string> {
   if (process.env.DEV) return resolve("assets");
 
-  const pkgJsonPath = await import.meta.resolve("@aklinker1/viteshot/package.json");
+  const pkgJsonPath = await import.meta
+    .resolve("@aklinker1/viteshot/package.json");
   return join(dirname(pkgJsonPath), "assets");
 }
