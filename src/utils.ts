30697a9b00fe4a7131901923a0dad3d4bb7a1781
Aaron <aaronklinker1@gmail.com>
2026-04-21 13:21:29 -0500

    Fix formatting in getViteshotAssetsDir function

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import { resolve, dirname, join } from "node:path";
 
 export async function getViteshotAssetsDir(): Promise<string> {
   if (process.env.DEV) return resolve("assets");
-  
+
   const pkgJsonPath = await import.meta.resolve("viteshot/package.json");
   return join(dirname(pkgJsonPath), "assets");
 }
